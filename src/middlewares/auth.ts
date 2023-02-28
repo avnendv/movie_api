91c4c1dac948edfd4d325e790f3b421d1ad201ca
viet.nd <viet.nd@beetechsoft.vn>
2023-02-28 17:20:20 +0700

    add role middlewate

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,18 +1,40 @@
 import { NextFunction, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { JwtPayloadIF, RequestIF } from '../models';
+import { errorResponse } from '../utils';
+import { JwtPayloadIF, MessagePayLoad, RequestIF } from '../models';
 
-export const verifyToken = (req: RequestIF, res: Response, next: NextFunction) => {
+export const verifyToken = (req: RequestIF, _res: Response, next: NextFunction) => {
   const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-  if (!token) {
-    return res.status(403).send('A token is required for authentication');
-  }
   try {
+    if (!token) {
+      throw { msg: 'A token is required for authentication!' };
+    }
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET as string) as JwtPayloadIF;
     req.userId = decoded.id;
-  } catch (err) {
-    return res.status(401).send('Invalid Token');
+  } catch (error) {
+    const err = error as MessagePayLoad;
+    if (!err.msg) {
+      err.msg = 'Invalid Token!';
+    }
+    next(errorResponse(err));
   }
   return next();
+};
+
+export const roles = (roles: number[]) => {
+  return function (req: RequestIF, _res: Response, next: NextFunction) {
+    try {
+      if (!req.userId) {
+        throw { msg: 'Invalid user!' };
+      }
+      const role = 1; // role of user
+      if (!roles.includes(role)) {
+        throw { msg: 'Permission not allowed!' };
+      }
+      return next();
+    } catch (error) {
+      next(errorResponse(error));
+    }
+  };
 };
